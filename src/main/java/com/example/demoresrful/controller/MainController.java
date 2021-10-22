6fe6440464f1d8104977687bcdd0b21330f812f2
Lastzlo <z.last.zlo@gmail.com>
2021-10-22 16:52:51 +0300

    add CrossOrigin

--- src/main/java/com/example/demoresrful/controller/MainController.java
+++ src/main/java/com/example/demoresrful/controller/MainController.java
@@ -2,10 +2,12 @@ package com.example.demoresrful.controller;
 
 import com.example.demoresrful.entity.Task;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class MainController {
 
     @GetMapping("/")
